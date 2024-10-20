bb81387e4241fe8ec078f7035fe88c40c4e12e79
BelenG <90286485+SBelenB@users.noreply.github.com>
2024-10-20 03:36:39 -0300

    Initial backend project

---
+++ backend/src/test/java/com/involvedigital/involve/InvolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.involvedigital.involve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
